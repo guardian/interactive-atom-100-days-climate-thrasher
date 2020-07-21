11531b6d8e0fd952787b86bf85f7a7b0aa19b42f
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-07-21 13:07:26 +0200

    work in progress

--- atoms/compact/client/js/main.js
+++ atoms/compact/client/js/main.js
@@ -32,3 +32,75 @@ if (
     window.resize();
   }, 100);
 }
+
+setTimeout(() => {
+  updateCountdown(3);
+
+}, 5000);
+let countdownAnimation;
+
+function updateCountdown(n) {
+  const countdownEl = document.querySelector('.hundred-days__headline__line.number');
+  countdownEl.dataset.target = n;
+
+
+  requestAnimationFrame(animateCountdown);
+
+}
+
+function animateCountdown() {
+  const countdownEl = document.querySelector('.hundred-days__headline__line.number');
+  const currentNum = parseInt(countdownEl.innerText);
+  const targetN = countdownEl.dataset.target;
+  const delta = currentNum - targetN;
+
+  let nextFrame;
+  if (delta > 60) {
+    nextFrame = 30;
+    console.log('over 40', currentNum, targetN);
+
+  } else if (delta > 30) {
+    nextFrame = 60;
+    console.log('over 20', currentNum, targetN);
+
+  } else if (delta > 15) {
+    nextFrame = 120;
+    console.log('over 10', currentNum, targetN);
+
+  } else if (delta > 4) {
+    nextFrame = 180;
+    console.log('over 5', currentNum, targetN);
+
+  } else if (delta == 4) {
+    nextFrame = 240;
+    console.log('at 4', currentNum, targetN);
+
+  } else if (delta == 3) {
+    nextFrame = 360;
+    console.log('at 3', currentNum, targetN);
+
+  } else if (delta == 2) {
+    nextFrame = 520;
+    console.log('at 2', currentNum, targetN);
+
+  } else {
+    nextFrame = 520;
+    console.log('last 2', currentNum, targetN);
+
+  }
+
+  if (currentNum > targetN) {
+    countdownEl.innerText = currentNum - 1;
+    setTimeout(() => {
+      animateCountdown();
+    }, nextFrame);
+  } else {
+    // debug-only
+    setTimeout(() => {
+      countdownEl.innerText = 100;
+      animateCountdown();
+    }, 10000);
+  }
+  // countdownAnimation = requestAnimationFrame(updateCountdown);
+
+}
